9b518d21bd83dbbfd4a72bba78ffcfbb078c32f8
Matt Locklin <mattlocklin@live.co.uk>
2019-11-04 14:40:44 +0000

    Finished MVP and a quick attempt at a timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [lionScore, setLionScore] = React.useState(0)
+  const [tigersScore, setTigersScore] = useState(0)
+  const [time, setTime] = useState(0);
+  const secondsPassed = useRef(0);
+  const minutesPassed = useRef(0);
 
+  useEffect(() => {
+      setTimeout(() => {
+        secondsPassed.current = secondsPassed.current + 1;
+        if (secondsPassed.current === 60) {
+          minutesPassed.current = minutesPassed.current + 1;
+          secondsPassed.current = 0;
+        }
+        setTime(secondsPassed.current)
+    }, 1000)
+  })
+    
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,12 +31,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{lionScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className='timer'>{minutesPassed.current}{' '}:{' '}{time}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +44,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setLionScore(lionScore + 6)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setLionScore(lionScore + 2)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setTigersScore(tigersScore + 6)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setTigersScore(tigersScore + 2)}>Away Field Goal</button>
         </div>
       </section>
     </div>
